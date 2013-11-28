0d659d7a27ce1e3f59e915ef497b0e3c12cbd2f9
Yrii <asbim@mail.ru>
2013-11-28 15:16:42 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   root 'static_pages#home'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
