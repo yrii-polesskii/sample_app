eb828a7c24bb5bdc18fbd5e834ae4aa22544f78e
Yrii <asbim@mail.ru>
2013-11-30 14:41:50 +0700

    Finish user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
   def show
     @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
@@ -17,10 +22,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:sucess] = "User deleted."
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+       flash[:success]="Profile updated"
+       redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." 
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
